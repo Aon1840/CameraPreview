6a901588217198ab812b7e13640de56fd3cb4adc
Aon1840 <aonattapon1840.ap@gmail.com>
2019-02-22 11:07:47 +0700

    Add logic for take a picture

--- app/src/main/java/com/example/customcamera/MainActivity.java
+++ app/src/main/java/com/example/customcamera/MainActivity.java
@@ -3,14 +3,29 @@ package com.example.customcamera;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.SecureRandom;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = MainActivity.class.getName();
     private Camera mCamera;
@@ -45,6 +60,9 @@ public class MainActivity extends AppCompatActivity {
         if (rlCameraPreview != null) {
             rlCameraPreview.addView(mPreview);
         }
+
+        ivCapture.setOnClickListener(this);
+        ivFilter.setOnClickListener(this);
     }
 
     private void bindView() {
@@ -63,5 +81,164 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return c;
+    }
+
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            File pictureFile = getOutputMediaFile();
+            if (pictureFile == null){
+                Log.d(TAG, "Error creating media file, check storage permissions: ");
+                return;
+            }
+
+            MediaScannerConnection.scanFile(MainActivity.this,
+                    new String[] { pictureFile.toString() }, null,
+                    new MediaScannerConnection.OnScanCompletedListener() {
+                        public void onScanCompleted(String path, Uri uri) {
+                            mCamera.startPreview();
+                        }
+                    });
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+    private static File getOutputMediaFile(){
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "Bearman");
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                System.out.println("Directory not created");
+                return null;
+            }
+        }
+
+        SecureRandom random = new SecureRandom();
+        int num = random.nextInt(1000000);
+        return new File(mediaStorageDir.getAbsolutePath() + File.separator +
+                "IMG_"+ num + ".jpg");
+    }
+
+    public void colorEffectFilter(View v){
+        try {
+            Camera.Parameters parameters = mCamera.getParameters();
+            switch (v.getId()) {
+                case R.id.rlNone:
+                    parameters.setColorEffect(Camera.Parameters.EFFECT_NONE);
+                    mCamera.setParameters(parameters);
+                    break;
+                case R.id.rlAqua:
+                    parameters.setColorEffect(Camera.Parameters.EFFECT_AQUA);
+                    mCamera.setParameters(parameters);
+                    break;
+                case R.id.rlBlackBoard:
+                    parameters.setColorEffect(Camera.Parameters.EFFECT_BLACKBOARD);
+                    mCamera.setParameters(parameters);
+                    break;
+                case R.id.rlMono:
+                    parameters.setColorEffect(Camera.Parameters.EFFECT_MONO);
+                    mCamera.setParameters(parameters);
+                    break;
+                case R.id.rlNegative:
+                    parameters.setColorEffect(Camera.Parameters.EFFECT_NEGATIVE);
+                    mCamera.setParameters(parameters);
+                    break;
+                case R.id.rlPosterized:
+                    parameters.setColorEffect(Camera.Parameters.EFFECT_POSTERIZE);
+                    mCamera.setParameters(parameters);
+                    break;
+                case R.id.rlSepia:
+                    parameters.setColorEffect(Camera.Parameters.EFFECT_SEPIA);
+                    mCamera.setParameters(parameters);
+                    break;
+                case R.id.rlSolarized:
+                    parameters.setColorEffect(Camera.Parameters.EFFECT_SOLARIZE);
+                    mCamera.setParameters(parameters);
+                    break;
+                case R.id.rlWhiteBoard:
+                    parameters.setColorEffect(Camera.Parameters.EFFECT_WHITEBOARD);
+                    mCamera.setParameters(parameters);
+                    break;
+            }
+        }catch (Exception ex){
+            Log.d(TAG,ex.getMessage());
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ivFilter:
+                if (horizontalScrollView.getVisibility() == View.VISIBLE) {
+                    horizontalScrollView.setVisibility(View.GONE);
+                } else {
+                    horizontalScrollView.setVisibility(View.VISIBLE);
+                }
+                break;
+            case R.id.ivCapture:
+                mCamera.takePicture(null,null,mPicture);
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == PERMISSION_CALLBACK_CONSTANT){
+            boolean allgranted = false;
+            for(int i=0;i<grantResults.length;i++){
+                if(grantResults[i] == PackageManager.PERMISSION_GRANTED){
+                    allgranted = true;
+                } else {
+                    allgranted = false;
+                    break;
+                }
+            }
+
+
+            if(allgranted){
+                initial();
+            } else if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,Manifest.permission.CAMERA)){
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA},PERMISSION_CALLBACK_CONSTANT);
+                }
+            } else if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    },PERMISSION_CALLBACK_CONSTANT);
+                }
+            } else {
+                Toast.makeText(MainActivity.this,"Permission is mandatory, Try giving it from App Settings",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mCamera != null) {
+            mCamera.stopPreview();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mCamera != null) {
+            mCamera.release();
+            mCamera = null;
+        }
     }
 }
